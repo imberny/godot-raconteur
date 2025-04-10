698259cd288556163bf3b368bd43007ee184417e
imberny <bernard.p.cloutier@gmail.com>
2025-04-10 14:03:59 -0400

    Add nodes to beat scenario

--- tests/raconteur/test_raconteur_beat.gd
+++ tests/raconteur/test_raconteur_beat.gd
@@ -2,14 +2,26 @@ extends GutTest
 
 
 func test_beat():
-    var beat := RaconteurBeat.new()
+	var beat := RaconteurBeat.new()
 
-    beat.alias_add(&"merchant", &"character")
+	beat.alias_add(&"merchant", &"character")
 
-    beat.alias_add_new_entity(&"item", &"ring", {"name": &"Ring of Power", &"quality": &"superior"})
+	beat.alias_add_new_entity(&"item", &"ring", {"name": &"Ring of Power", &"quality": &"superior"})
 
-    beat.new_relationship(&"merchant", &"ring", &"owns")
+	beat.new_relationship(&"merchant", &"ring", &"owns")
 
-    beat.constraint_add(RaconteurConstraintHasTag.new(&"PROTAGONIST", &"shopping"))
-    
-    assert_true(true)
+	beat.constraint_add(RaconteurConstraintHasTag.new(&"PROTAGONIST", &"shopping"))
+
+	var start_node := beat.scenario_node_create()
+	beat.scenario_set_start_node(start_node.id)
+	start_node.name = &"start"
+	start_node.constraint_add(RaconteurConstraintHasTag.new(&"player", &"clever"))
+	var second_node := beat.scenario_node_create()
+	second_node.name = &"second"
+	var greetings_line := RaconteurLine.new()
+	greetings_line.label = &"greetings"
+	greetings_line.content = "Hello my fellow!"
+	second_node.line_add(greetings_line)
+	start_node.next_add(second_node)
+	
+	assert_true(true)
